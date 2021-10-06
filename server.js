c051dc9e8308a9f3955fb5c756ce11e783b63ffb
Oleksandr Kravchuk <o.m.kravchuk19@gmail.com>
2021-10-06 11:43:44 +0300

    check api call

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.static(__dirname + 'build'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + 'build/index.html'));
-  console.log('start get')
+  console.log('start gets')
 });
 
 const optionsCors = {
