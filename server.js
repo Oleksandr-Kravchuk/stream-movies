9c6f14cb418d1889b6f3c66665aa98a88cfead12
Oleksandr Kravchuk <o.m.kravchuk19@gmail.com>
2021-10-06 21:20:19 +0300

    fix

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const {
 
 app.use(express.static(__dirname + 'build'));
 
-app.get('*', function (req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname + 'build/index.html'));
 });
 
